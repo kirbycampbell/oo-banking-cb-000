e87a5262cba6a4d2bf8701f5ce3cbcdb9a6f542f
Kirby Campbell <jkirbycampbell@gmail.com>
2018-05-10 17:40:26 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -17,7 +17,7 @@ attr_accessor :sender, :receiver, :amount, :status
   end
 
   def execute_transaction
-    if self.valid? == true
+    if self.valid? == true && sender.balance >= @amount
       sender.balance -= @amount
       receiver.balance += @amount
       @status = 'complete'
